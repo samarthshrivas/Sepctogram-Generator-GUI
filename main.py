d1a563ec324ecb42fb2bbee623ac5c7c332b6c50
SamarthShrivas <SamarthShrivas@users.noreply.github.com>
2023-08-17 19:52:00 +0530

    Update

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import librosa
+import matplotlib.pyplot as plt
+
+
+st.set_page_config(page_title="SpecGen", page_icon=":📈:", layout="wide")
+st.title(":microphone: Lofi Converter")
+st.info("Tip: Use Headphone for best experience :headphones:")
+
+
+def genspec():
+    return 
+
